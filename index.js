9d41791f89a8bb8c56855dae284d04b9670b0d36
Miss Nalgas <sebasar997@gmail.com>
2021-08-16 20:03:42 -0500

    Added dictionaries and routes in API

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const port = 3001;
 const myengine = require("./templates");
 const mainRouter = require("./routes");
 
+const { I18n } = require("i18n");
+const path = require("path");
+
 app.engine("html", myengine);
 app.set("views", "./views");
 app.set("view engine", "html");
 
 app.use(express.static("static"));
+const i18n = new I18n({
+    locales: ["en", "es", "ru"],
+    directory: path.join(__dirname, "locales")
+});
+app.use(i18n.init);
+
+
 app.use(mainRouter);
 
 app.listen(port, () => {
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 
 router.get("/locale", (req, res) => {
+    res.json({locale: req.getLocale()});
+})
+
+router.get("/catalog", (req, res) => {
+    res.json(req.getCatalog());
+})
+
+router.post("/secret", (req, res) => {
     res.send("locale");
 })
 
